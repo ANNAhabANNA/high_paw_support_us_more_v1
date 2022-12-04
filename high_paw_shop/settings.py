8446754725e930d3a51d9b7b420017ba97662714
Anna <akozorkova@gmail.com>
2022-12-04 13:46:12 +0000

    Add allauth authentication functionality upon registration for the website and create templates/allauth folders

--- high_paw_shop/settings.py
+++ high_paw_shop/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -37,6 +37,15 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # Used by the social account app to create the proper callback URLs when connecting via social media accounts
+    # Planned to be used in future features to high_paw_support_us_more_v1 project
+    'django.contrib.sites',
+    'allauth',
+    # Allows all the basic user account functionality (logging in/out, password reset, user registration)
+    'allauth.account',
+    # Handles logging in via social media providers
+    'allauth.socialaccount',
+
 ]
 
 MIDDLEWARE = [
@@ -59,6 +68,7 @@ TEMPLATES = [
         'OPTIONS': {
             'context_processors': [
                 'django.template.context_processors.debug',
+                # Allows to access the HTTP request object in templates
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
@@ -66,6 +76,34 @@ TEMPLATES = [
         },
     },
 ]
+
+AUTHENTICATION_BACKENDS = [
+    # Needed to login by username in Django admin, regardless of `allauth`
+    # Handles superuser logging
+    'django.contrib.auth.backends.ModelBackend',
+
+    # `allauth` specific authentication methods, such as login by e-mail
+    'allauth.account.auth_backends.AuthenticationBackend',
+]
+
+# Used by the social account app to create the proper callback URLs when connecting via social media accounts
+SITE_ID = 1
+
+# Temporarily logs confirmation emails to any new accounts to the console so that the confirmation links can be obtained
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+# Tells allauth to allow authentication using either usernames or emails
+# Configures email to be required to register for the site
+ACCOUNT_AUTHENTICATION_METHOD = 'username_email'
+ACCOUNT_EMAIL_REQUIRED = True
+# Configures email verification to be mandatory to avoid fake accounts
+ACCOUNT_EMAIL_VERIFICATION = 'mandatory'
+# Configures email entry two times to avoid typos
+ACCOUNT_SIGNUP_EMAIL_ENTER_TWICE = True
+ACCOUNT_USERNAME_MIN_LENGTH = 4
+# Specifies a login url and a url to redirect back to after logging in
+LOGIN_URL = '/accounts/login/'
+LOGIN_REDIRECT_URL = '/'
 
 WSGI_APPLICATION = 'high_paw_shop.wsgi.application'
 
