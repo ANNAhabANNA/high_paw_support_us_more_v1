8cef9ed7f179e6c8e64da596cd224535a5d41522
Anna <akozorkova@gmail.com>
2022-12-11 15:09:38 +0000

    Create add product view for shopping bag functionality

--- bag/urls.py
+++ bag/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.review_bag, name='review_bag')
+    path('', views.review_bag, name='review_bag'),
+    path('add/<item_id>/', views.add_product, name='add_product'),
 ]
--- bag/views.py
+++ bag/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 # Create your views here.
 
@@ -6,3 +6,25 @@ def review_bag(request):
     """ A view that renders the bag contents page """
 
     return render(request, 'bag/bag.html')
+
+
+def add_product(request, item_id):
+    """ Add a quantity of the specified product to the shopping bag """
+
+    # Gets quantity from the view.
+    quantity = int(request.POST.get('quantity'))
+
+    redirect_url = request.POST.get('redirect_url')
+
+    # Allows us to store the contents of the shopping bag in the HTTP session.
+    bag = request.session.get('bag', {})
+
+    if item_id in list(bag.keys()):
+        bag[item_id] += quantity
+    else:
+        bag[item_id] = quantity
+
+    # Overwrite the variable in the session with the updated version.
+    request.session['bag'] = bag
+    print(request.session['bag'])
+    return redirect(redirect_url)
