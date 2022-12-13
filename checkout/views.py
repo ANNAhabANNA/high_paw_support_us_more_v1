ae4c6981d8ac10e64f4a3e6499d6a6a97aa71ee6
Anna <akozorkova@gmail.com>
2022-12-13 23:34:25 +0000

    Create stripe element in checkout template

--- checkout/views.py
+++ checkout/views.py
@@ -16,6 +16,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51M8NZkHHB04CHoTNxQeatnHbaTu5fIOEAm0M9ThyLipfvwmilhQPS0viK8h5UTwITvuZCcDHj6jet2EST1yxKSD400Vq1uaAXJ',
+        'client_secret': 'test client secret',
     }
 
     return render(request, template, context)
