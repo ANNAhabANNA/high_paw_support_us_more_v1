b15bd662d9294bfa096aff6df536ff2dad679e02
Anna <akozorkova@gmail.com>
2022-12-17 16:12:39 +0000

    Update user profile form, correct font color for country selection

---
+++ profiles/static/profiles/js/countryfield.js
@@ -0,0 +1,13 @@
+// Changes country field font color after user selection.
+let countrySelected = $('#id_default_country').val();
+if(!countrySelected) {
+    $('#id_default_country').css('color', 'grey');
+};
+$('#id_default_country').change(function() {
+    countrySelected = $(this).val();
+    if(!countrySelected) {
+        $(this).css('color', 'grey');
+    } else {
+        $(this).css('color', '#000');
+    }
+});
