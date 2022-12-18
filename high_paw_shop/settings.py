1fa3a2ebeb9e95128e7f13f714565b5e7c84a2f0
Anna <akozorkova@gmail.com>
2022-12-18 18:15:24 +0000

    Remove secret key and set debug

--- high_paw_shop/settings.py
+++ high_paw_shop/settings.py
@@ -21,10 +21,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-f-dr%qd0jzk#kyw4d&a&%e9rv&3taq*oqol1zd2z91eldf@v!s'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['highpawsupportusmore.herokuapp.com', 'localhost']
 
