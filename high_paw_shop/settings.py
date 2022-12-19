101f63c407cb51cb3b5cabf3d648fc1022e6469c
Anna <akozorkova@gmail.com>
2022-12-19 00:01:54 +0000

    Add favicon and subscription link

--- high_paw_shop/settings.py
+++ high_paw_shop/settings.py
@@ -21,12 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '#72#(p--e=u6lmea#59ys&w&0drl9^t(tww2h#@fdysy-hhj_g'
-#SECRET_KEY = os.environ.get('SECRET_KEY', '')
+
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-#DEBUG = 'DEVELOPMENT' in os.environ
+#DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['highpawsupportusmore.herokuapp.com', 'localhost']
 
