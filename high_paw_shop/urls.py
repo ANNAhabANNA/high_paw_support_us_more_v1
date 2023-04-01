8ca234b326eb75ddf1a68f85caa8721143a6c314
Anna <akozorkova@gmail.com>
2023-04-01 21:37:18 +0000

    Revert "Add blog url to project urls.py"
    This reverts commit 097e63f32b8feec4ac40fcc8e01dcad6aaf56a2d.

--- high_paw_shop/urls.py
+++ high_paw_shop/urls.py
@@ -41,8 +41,6 @@ urlpatterns = [
     path('reviews/', include('reviews.urls')),
     # Subscription page
     path('subscription/', include('marketing.urls')),
-    # Blog page
-    path('blog/', include('blog.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 handler403 = 'high_paw_shop.views.custom_403_error'
