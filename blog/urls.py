ec261b1ba7c16244854c09df8c1b6e57e5be7bff
Anna <akozorkova@gmail.com>
2023-04-01 21:37:38 +0000

    Revert "Create blog views to render blog.html"
    This reverts commit 378c77ff08a0befacde3a5c217a63befae3cb2b1.

--- blog/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .import views
-from .views import BlogList
-
-
-urlpatterns = [
-    path('', BlogList.as_view(), name='blog'),
-]
--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,3 @@
 from django.shortcuts import render
-from django.views import generic
-from .models import BlogPost
-from django.views.generic import ListView
 
-
-class BlogList(ListView):
-    """Creates the post on the blog"""
-    model = BlogPost
-    template_name = 'blog/blog.html'
+# Create your views here.
