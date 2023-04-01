9f74bb0aa039e6f594162a8745943d839225ca71
Anna <akozorkova@gmail.com>
2023-04-01 22:03:27 +0000

    Create blog views that render blog posts

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
+from .models import BlogPost, Comment
 
-# Register your models here.
+
+@admin.register(BlogPost)
+class BlogPostAdmin(admin.ModelAdmin):
+
+    list_display = ('tag', 'slug', 'status', 'created_on')
+    search_fields = ['tag', 'text']
+    list_filter = ('status', 'created_on')
+    prepopulated_fields = {'slug': ('tag',)}
+    post_fields = ('text',)
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('name', 'body', 'post', 'generated_on', 'approved')
+    list_filter = ('approved', 'generated_on')
+    search_fields = ('name', 'email', 'body')
+    actions = ['approve_comments']
+
+    def approve_comments(self, request, queryset):
+        queryset.update(approved=True)
