8661e2c310bd9f68e93bd3812eec9cec7f7935d7
Anna <akozorkova@gmail.com>
2023-04-02 13:38:00 +0000

    Create comment form for blog post

--- blog/views.py
+++ blog/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, get_object_or_404
 from django.views import generic, View
 from .models import BlogPost
 from django.views.generic import ListView
+from .forms import CommentBlogForm
 
 
 class BlogList(ListView):
@@ -27,6 +28,39 @@ class PostDetail(View):
             {
                 "post": post,
                 "comments": comments,
+                "commented": False,
+                "liked": liked,
+                'comment_form': CommentBlogForm()
+            },
+        )
+    
+    def post(self, request, slug, *args, **kwargs):
+
+        blogpost_collection = BlogPost.objects.filter(status=1)
+        post = get_object_or_404(blogpost_collection, slug=slug)
+        comments = post.comments.filter(approved=True).order_by("-generated_on")
+        liked = False
+        if post.likes.filter(id=self.request.user.id).exists():
+            liked = True
+
+        comment_form = CommentBlogForm(data=request.POST)
+        if comment_form.is_valid():
+            comment_form.instance.email = request.user.email
+            comment_form.instance.name = request.user.username
+            comment = comment_form.save(commit=False)
+            comment.post = post
+            comment.save()
+        else:
+            comment_form = CommentBlogForm()
+
+        return render(
+            request,
+            "blog/post_detail.html",
+            {
+                "post": post,
+                "comments": comments,
+                "commented": True,
+                "comment_form": comment_form,
                 "liked": liked
             },
         )
